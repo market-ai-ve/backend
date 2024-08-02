96c6211ff66dfa1bc2f6b1ecf1cbd4eab017ff68
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-08-02 11:10:02 -0400

    test: test to buyer person entity

--- src/components/marketing-content/domain/entities/buyerPerson/buyerPerson.ts
+++ src/components/marketing-content/domain/entities/buyerPerson/buyerPerson.ts
@@ -1,5 +1,16 @@
 export class BuyerPerson {
-  readonly companyName: string;
-  readonly companyDescription: string;
-  readonly dataSearch?: string[];
+  constructor(
+    readonly companyName: string,
+    readonly companyDescription: string,
+    readonly dataSearch?: string[]
+  ) {
+    // validate data
+    if (typeof this.companyName !== 'string') throw new TypeError('parameter companyName is not string')
+    if (typeof this.companyDescription !== 'string') throw new TypeError('parameter companyDescription is not string')      
+    if (!Array.isArray(this.dataSearch)) throw new TypeError('parameter dataSearch is not array')
+  }
+
+  toString() {
+    return `company name: ${this.companyName} \n\ncompany description: ${this.companyDescription} \n\n data search: ${this.dataSearch.join(', ')}`
+  }
 }
