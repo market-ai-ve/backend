76189592987017a22782146ee744f6d49a7245ba
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-08-02 11:10:18 -0400

    style: format

--- src/components/marketing-content/domain/entities/buyerPerson/buyerPerson.spec.ts
+++ src/components/marketing-content/domain/entities/buyerPerson/buyerPerson.spec.ts
@@ -1,63 +1,76 @@
-import { BuyerPerson } from "./buyerPerson"
+import { BuyerPerson } from './buyerPerson';
 
 describe('TestCas entity buyer person', () => {
-  
-    beforeAll(() => {
-        let entity: BuyerPerson
-    })
+  beforeAll(() => {
+    let entity: BuyerPerson;
+  });
 
-    test('Should return class with passed success params', () => {
-        const companyName = 'Company Name'
-        const companyDescription = 'Company Description'
-        const dataSearch = ['Tag 01', 'Tag 02']
-        
-        const buyerPerson = new BuyerPerson(companyName, companyDescription, dataSearch)
+  test('Should return class with passed success params', () => {
+    const companyName = 'Company Name';
+    const companyDescription = 'Company Description';
+    const dataSearch = ['Tag 01', 'Tag 02'];
 
-        expect(buyerPerson.toString()).toEqual(`company name: ${companyName} \n\ncompany description: ${companyDescription} \n\n data search: ${dataSearch.join(', ')}`)
-    })
+    const buyerPerson = new BuyerPerson(
+      companyName,
+      companyDescription,
+      dataSearch,
+    );
 
-    test('Should return error when passing the companyName parameter as data other than string', () => {
-        const companyName = 1
-        const companyDescription = 'Company Description'
-        const dataSearch = ['Tag 01', 'Tag 02']
+    expect(buyerPerson.toString()).toEqual(
+      `company name: ${companyName} \n\ncompany description: ${companyDescription} \n\n data search: ${dataSearch.join(', ')}`,
+    );
+  });
 
-        expect(() => {
-        // @ts-ignore
-            return new BuyerPerson(companyName, companyDescription, dataSearch)
-        }).toThrow(TypeError)
-        expect(() => {
-        // @ts-ignore
-            return new BuyerPerson(companyName, companyDescription, dataSearch)
-        }).toThrow('parameter companyName is not string')
-    })
+  test('Should return error when passing the companyName parameter as data other than string', () => {
+    const companyName = 1;
+    const companyDescription = 'Company Description';
+    const dataSearch = ['Tag 01', 'Tag 02'];
 
-    test('Should return error when passing the companyDescription parameter as data other than string', () => {
-        const companyName = 'Company name'
-        const companyDescription = 1
-        const dataSearch = ['Tag 01', 'Tag 02']
+    expect(() => {
+      // @ts-ignore
+      return new BuyerPerson(companyName, companyDescription, dataSearch);
+    }).toThrow(TypeError);
+    expect(() => {
+      // @ts-ignore
+      return new BuyerPerson(companyName, companyDescription, dataSearch);
+    }).toThrow('parameter companyName is not string');
+  });
 
-        expect(() => {
-        // @ts-ignore
-            return new BuyerPerson(companyName, companyDescription, dataSearch)
-        }).toThrow(TypeError)
-        expect(() => {
-        // @ts-ignore
-            return new BuyerPerson(companyName, companyDescription, dataSearch)
-        }).toThrow('parameter companyDescription is not string')
-    })
+  test('Should return error when passing the companyDescription parameter as data other than string', () => {
+    const companyName = 'Company name';
+    const companyDescription = 1;
+    const dataSearch = ['Tag 01', 'Tag 02'];
 
-    test('Should return error when passing the dataSearch parameter as data other than Array', () => {
-        const companyName = 'Company name'
-        const companyDescription = 'Company Description'
-        const dataSearch = 1
-                
-        expect(() => {
-            // @ts-ignore
-            return new BuyerPerson(companyName, companyDescription, dataSearch).toString()
-        }).toThrow(TypeError)
-        expect(() => {
-            // @ts-ignore
-            return new BuyerPerson(companyName, companyDescription, dataSearch).toString()
-        }).toThrow('parameter dataSearch is not array')
-    })
-})
+    expect(() => {
+      // @ts-ignore
+      return new BuyerPerson(companyName, companyDescription, dataSearch);
+    }).toThrow(TypeError);
+    expect(() => {
+      // @ts-ignore
+      return new BuyerPerson(companyName, companyDescription, dataSearch);
+    }).toThrow('parameter companyDescription is not string');
+  });
+
+  test('Should return error when passing the dataSearch parameter as data other than Array', () => {
+    const companyName = 'Company name';
+    const companyDescription = 'Company Description';
+    const dataSearch = 1;
+
+    expect(() => {
+      // @ts-ignore
+      return new BuyerPerson(
+        companyName,
+        companyDescription,
+        dataSearch,
+      ).toString();
+    }).toThrow(TypeError);
+    expect(() => {
+      // @ts-ignore
+      return new BuyerPerson(
+        companyName,
+        companyDescription,
+        dataSearch,
+      ).toString();
+    }).toThrow('parameter dataSearch is not array');
+  });
+});
--- src/components/marketing-content/domain/entities/buyerPerson/buyerPerson.ts
+++ src/components/marketing-content/domain/entities/buyerPerson/buyerPerson.ts
@@ -2,15 +2,18 @@ export class BuyerPerson {
   constructor(
     readonly companyName: string,
     readonly companyDescription: string,
-    readonly dataSearch?: string[]
+    readonly dataSearch?: string[],
   ) {
     // validate data
-    if (typeof this.companyName !== 'string') throw new TypeError('parameter companyName is not string')
-    if (typeof this.companyDescription !== 'string') throw new TypeError('parameter companyDescription is not string')      
-    if (!Array.isArray(this.dataSearch)) throw new TypeError('parameter dataSearch is not array')
+    if (typeof this.companyName !== 'string')
+      throw new TypeError('parameter companyName is not string');
+    if (typeof this.companyDescription !== 'string')
+      throw new TypeError('parameter companyDescription is not string');
+    if (!Array.isArray(this.dataSearch))
+      throw new TypeError('parameter dataSearch is not array');
   }
 
   toString() {
-    return `company name: ${this.companyName} \n\ncompany description: ${this.companyDescription} \n\n data search: ${this.dataSearch.join(', ')}`
+    return `company name: ${this.companyName} \n\ncompany description: ${this.companyDescription} \n\n data search: ${this.dataSearch.join(', ')}`;
   }
 }
