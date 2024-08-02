25153d8c204569c6bdc5bd9426f2f2710931cf1a
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-08-02 11:27:43 -0400

    style: format

--- src/components/marketing-content/domain/interfaces/IBuyerPerson.ts
+++ src/components/marketing-content/domain/interfaces/IBuyerPerson.ts
@@ -1,21 +1,21 @@
 // Primity Values
 export interface PrimitiviBuyerPerson {
-    companyName: string,
-    companyDescription: string,
-    dataSearch?: string[],
-    searchTerms: string[]
+  companyName: string;
+  companyDescription: string;
+  dataSearch?: string[];
+  searchTerms: string[];
 }
 
 // Data compuesta
-export type ICreateBuyerPerson = Omit<PrimitiviBuyerPerson, 'searchTerms'>
-export type IReturnCreatePerson = Pick<PrimitiviBuyerPerson, 'searchTerms'>
+export type ICreateBuyerPerson = Omit<PrimitiviBuyerPerson, 'searchTerms'>;
+export type IReturnCreatePerson = Pick<PrimitiviBuyerPerson, 'searchTerms'>;
 
 // Repository
 export interface BuyerRepository {
-    create(payload: ICreateBuyerPerson): ICreateBuyerPerson
+  create(payload: ICreateBuyerPerson): ICreateBuyerPerson;
 }
 
 // IA Generate Text
 export interface CreateBuyerPersonAI {
-    create(payload: ICreateBuyerPerson): IReturnCreatePerson
+  create(payload: ICreateBuyerPerson): IReturnCreatePerson;
 }
