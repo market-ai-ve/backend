9f9745f09270e5955aaa5a9a3794bc24373053aa
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-08-02 01:15:09 -0400

    feat: install & config eslit to use order import

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,21 +5,55 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
+  plugins: ['@typescript-eslint', 'prettier', 'import'], 
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+
+    // Config import
+     // turn on errors for missing imports
+    'import/no-unresolved': 'error',
+     // 'import/no-named-as-default-member': 'off',
+    'import/order': [
+         'error',
+         {
+           groups: [
+             'builtin', // Built-in imports (come from NodeJS native) go first
+             'external', // <- External imports
+             'internal', // <- Absolute imports
+             ['sibling', 'parent'], // <- Relative imports, the sibling and parent types they can be mingled together
+             'index', // <- index imports
+             'unknown', // <- unknown
+           ],
+           'newlines-between': 'always',
+           alphabetize: {
+             /* sort in ascending order. Options: ["ignore", "asc", "desc"] */
+             order: 'asc',
+             /* ignore case. Options: [true, false] */
+             caseInsensitive: true,
+           },
+         },
+       ],
   },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
+      },
+    },
 };
