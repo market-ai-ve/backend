ed4fcf5d964e77c306c3ab1148c64f11d622a30b
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-08-03 16:08:31 -0400

    fix: remove file initial app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
 import { ContentsModule } from './components/marketing-content/infraestructure/contents.module';
 
 @Module({
   imports: [ContentsModule],
-  controllers: [AppController],
-  providers: [],
 })
 export class AppModule {}
