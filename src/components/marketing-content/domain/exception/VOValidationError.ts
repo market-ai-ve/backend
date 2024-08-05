153ce875fe2554f050ad01d28b5f694f6d991f61
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-08-04 22:05:32 -0400

    docs: add docString to exception

--- src/components/marketing-content/domain/exception/VOValidationError.ts
+++ src/components/marketing-content/domain/exception/VOValidationError.ts
@@ -1,16 +1,24 @@
+/**
+ * Custom error class for handling validation errors in value objects.
+ */
 export class VOValidationError extends Error {
   constructor(errors: any) {
     let err = '';
-    for (const error of errors) {
-      const contraints = error.constraints;
-      const keys = Object.keys(contraints);
-
-      err = contraints[keys[0]];
-      break;
+    if (errors && errors.length > 0) {
+      for (const error of errors) {
+        const contraints = error.constraints;
+        if (contraints) {
+          const keys = Object.keys(contraints);
+          if (keys.length > 0) {
+            err = contraints[keys[0]];
+            break;
+          }
+        }
+      }
     }
 
     super(err);
-    // Set the prototype explicitly.
-    Object.setPrototypeOf(this, VOValidationError.prototype);
+    // Use Error.captureStackTrace for better stack trace management.
+    Error.captureStackTrace(this, VOValidationError);
   }
 }
